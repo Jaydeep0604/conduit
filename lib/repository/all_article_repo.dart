69583767e545db9ac47c494fa06bea03b4339e55
Jaydeep0604 <jaydeepkoladiya64@gmail.com>
2023-09-24 19:32:27 +0530

    test

--- lib/repository/all_article_repo.dart
+++ lib/repository/all_article_repo.dart
@@ -454,7 +454,9 @@ class AllArticlesImpl extends AllArticlesRepo {
     if (response.statusCode == 200) {
       return true;
     } else {
-      throw Exception('Failed to unfollow user');
+      throw Exception(
+        'Failed to unfollow user',
+      );
     }
   }
 }
